0cb1c1b46bdf31ed64a6489c8e652863e950108e
Norb <norb@norb.com>
2019-04-09 23:15:04 +0200

    Dodaj pliki projektów.

---
+++ SchoolRegister.BLL/Entities/GradeScale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+    public enum GradeScale
+    {
+        NDST = 2,
+        DST,
+        DB,
+        BDB
+    }
+}
